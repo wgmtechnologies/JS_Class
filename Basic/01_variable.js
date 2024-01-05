6c9dd36af6afefc25ce0b0d13c81bbe7e5270eaf
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-05 13:32:26 +0000

    Variables

---
+++ Basic/01_variable.js
@@ -0,0 +1,13 @@
+const customerID = 7778787;
+let password = "wgmtech@222";
+let confirmPassword = password;
+var eMail = "abc@gmail.com";
+eMail1 = "aaaa@gmail.com";
+
+console.log(customerID);
+console.log(password);
+console.log(confirmPassword);
+console.log(eMail);
+console.log(eMail1);
+
+
