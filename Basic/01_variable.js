0cde4909a8500e9c6187852661eea1611612946e
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-06 08:12:46 +0530

    Update 01_variable.js

--- Basic/01_variable.js
+++ Basic/01_variable.js
@@ -11,3 +11,57 @@ console.log(eMail);
 console.log(eMail1);
 
 
+
+
+ // latest version  "use strict";
+
+// document.write(typeof 11)
+
+// var | Let | Const
+
+
+/*
+const customerID = 909090909;
+const passpword = "abs@1233";
+const confirmPassword = passpword;
+
+console.log(customerID);
+console.log(passpword);
+console.log(confirmPassword);
+
+ Not Accept in var => 0-9 and any special char. 
+
+const $name1 = "Umesh";  // _ $  
+const firstName = "Umesh";
+
+console.log($name1);
+
+*/
+
+
+// const UserID = "Umesh";
+// const UserID = "Ramesh";
+
+
+// let UserID = "Umesh";
+// UserID = "Ramesh";
+
+
+// const password = 1212;
+// confirmPassword = password
+
+
+
+// var password = 1212;
+// confirmPassword = password
+
+// var firstName = "Umesh";
+// lastName = "Kumar";
+
+
+
+// console.log(firstName, lastName);
+
+
+
+
