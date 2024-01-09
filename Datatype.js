381ff7f2d02d3ef7998fa35cbb78c413d3506452
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-09 08:13:28 +0530

    DateType

---
+++ Datatype.js
@@ -0,0 +1,110 @@
+// JavaScript Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+// var a = 100.50;
+// var b = 200;
+// c = a+b;
+
+
+// console.log(typeof a, a);
+// console.log(typeof c)
+
+
+// String
+
+// let firstName = "Umesh Kumar";
+// designation = "frontEnd Developer";
+
+// console.log(typeof firstName, firstName)
+// console.log("Hi am", firstName, designation)
+
+
+// BigInt
+// let x = BigInt("123456789012345678901234567890");
+
+// let a = 100n;
+// let b = BigInt(200);
+// let c = a+b;
+
+// const previousMaxSafe = BigInt(Number.MAX_SAFE_INTEGER);
+// console.log(previousMaxSafe)
+
+// console.log(typeof c, c);
+
+
+
+// Booleans
+// let x = true; 
+// let y = false;
+
+// console.log(typeof x);
+
+
+
+// let actualPrice = 1200;
+// let salePrice = 250;
+
+// let result = (actualPrice-salePrice)/actualPrice*100;
+
+// BODMAS
+
+// console.log(result)
+
+
+
+// let customerId = "rms0011";
+// let password = "ramesh@0990";
+// let confirmPassword = password;
+
+// console.log(password)
+// console.log(confirmPassword)
+
+// undefined  |  Null
+
+// let x;
+// console.log(x)
+
+// let x = null;
+// console.log(typeof x, x)
+
+
+
+
+
+
+
+
+
+
+
+// Object Datatype
+// 1. An object
+// 2. An array
+// 3. A date
+
+
+// Object:
+const person = {firstName:"John", lastName:"Kumar"};
+
+console.log(typeof person, person)
+console.log(person.firstName, person.lastName)
+
+
+
+// Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+// const date = new Date("2022-03-25");
