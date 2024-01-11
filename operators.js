edfeca9b8fcb7f6e16817745ead5bb699d822771
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-11 07:59:38 +0530

    M_Operator

---
+++ operators.js
@@ -0,0 +1,38 @@
+// let age  = "20";
+// console.log(age);
+// console.log(typeof age);
+
+// let newAge = Number(age);
+
+// console.log(typeof newAge)
+
+
+// let age = prompt("Enter your age","age");
+// let newAge = Number(age);
+// console.log(typeof newAge, newAge);
+
+
+// let age = parseInt(prompt("Enter your age","age"));
+// let age = Number(prompt("Enter your age","age"));
+// let age = +(prompt("Enter your age","age"));
+
+// let age = Number(prompt("Enter your age","age"));
+// let age = +(prompt("Enter your age","age"));
+// let age = parseFloat(prompt("Enter your age","age"));
+
+// console.log(typeof age, age)
+
+
+
+// let firstName = prompt("Enter your age","age");
+// NewName = firstName + " Kumar";
+
+// console.log(NewName);
+
+
+// let productPrice = +(prompt("Enter product price")); // 1200
+// let SellingPrice = +(prompt("Enter product price")); // 250
+
+// let DiscountOffer = ((productPrice-SellingPrice)/productPrice*100)+"%";
+
+// console.log(DiscountOffer);
