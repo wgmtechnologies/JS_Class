240e7e482a874151cc3e1521a84313f6cf4e62cc
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-20 16:58:59 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,38 +1,129 @@
-// let age  = "20";
-// console.log(age);
-// console.log(typeof age);
-
-// let newAge = Number(age);
-
-// console.log(typeof newAge)
-
-
-// let age = prompt("Enter your age","age");
-// let newAge = Number(age);
-// console.log(typeof newAge, newAge);
-
-
-// let age = parseInt(prompt("Enter your age","age"));
-// let age = Number(prompt("Enter your age","age"));
-// let age = +(prompt("Enter your age","age"));
-
-// let age = Number(prompt("Enter your age","age"));
-// let age = +(prompt("Enter your age","age"));
-// let age = parseFloat(prompt("Enter your age","age"));
-
-// console.log(typeof age, age)
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
 
 
 
-// let firstName = prompt("Enter your age","age");
-// NewName = firstName + " Kumar";
+// Arithmetic Operator	
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
 
-// console.log(NewName);
+
+// var value1 = 6;
+// var value2 = 5;
+// output=value1%value2
+// console.log(value1++) // Use and increment
+// console.log(value1)
+
+// console.log(++value1) // increment and use
+// console.log(value1++)
+// console.log(value1)
 
 
-// let productPrice = +(prompt("Enter product price")); // 1200
-// let SellingPrice = +(prompt("Enter product price")); // 250
 
-// let DiscountOffer = ((productPrice-SellingPrice)/productPrice*100)+"%";
+// Assignment Operators
 
-// console.log(DiscountOffer);
+// =	
+// +=	
+// -=	
+// *=	
+// /=	
+// %=	
+// **=
+
+
+// var a = 10;
+// a %= a
+// var b = a
+// console.log(b);
+
+
+
+
+// Comparison Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+
+
+
+// const password = "123";
+// confirmPassword = 123;
+
+// console.log(typeof password, password)
+// console.log(typeof confirmPassword, confirmPassword)
+
+// console.log(confirmPassword!==password)
+
+
+// var Hindi = 60;
+
+// if (Hindi<=60) {
+// 	console.log("Pass");
+// }else{
+// 	console.log("Fail");
+// }
+
+
+
+
+
+// Logical Operators
+// &&	logical and
+// ||	logical or
+// !	logical not
+
+
+
+
+var OpeningBalance = 1000;
+var PanCard = "Yes";
+var Adhaar = "Yes";
+
+result = OpeningBalance==1000 && PanCard=="Yes" && Adhaar=="Yes";
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// >50 "true", "False" total = >=180
+
+// var hindi = +prompt("Enter hindi Marks")
+// var eng = +prompt("Enter English Marks");
+// var math = +prompt("Enter Math Marks");
+// var total = hindi+eng+math;
+
+//result = hindi>50 && eng>50 && math>50 && total>=180; // all condition is true.  &&
+//result = (hindi>50 && eng>50 && math>50) || (total>=180); // && with ||
+
+
+
+// console.log(result)
