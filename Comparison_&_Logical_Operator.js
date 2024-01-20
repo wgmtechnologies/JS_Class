b2662288617d3c5bc640c3d573d0a4f0a13ae51a
wgmtechnologies <155529940+wgmtechnologies@users.noreply.github.com>
2024-01-20 08:04:57 +0530

    Comparison_&_Logical_Operator

---
+++ Comparison_&_Logical_Operator.js
@@ -0,0 +1,54 @@
+// Comparison Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+
+
+
+// var a = 10;
+// var b = 20;
+// // a=b;
+
+// console.log(a==b);
+
+
+// const password = "123";
+// confirmPassword = 123;
+
+// console.log(password!==confirmPassword);
+
+
+
+// var a = 101;
+// var b = 90;
+// console.log(a<b)
+
+
+
+
+
+// Logical Operators
+// &&	logical and
+// ||	logical or
+// !	logical not
+
+
+
+// >50 "true", "False" total = >=180
+
+// var hindi = +prompt("Enter hindi Marks")
+// var eng = +prompt("Enter English Marks");
+// var math = +prompt("Enter Math Marks");
+// var total = hindi+eng+math;
+
+//result = hindi>50 && eng>50 && math>50 && total>=180; // all condition is true.  &&
+//result = (hindi>50 && eng>50 && math>50) || (total>=180); // && with ||
+
+
+
+// console.log(result)
